619f18b490d51a828533e82b14132dff48408e68
João Victor de Assis <165854957+JoaoVictorASL@users.noreply.github.com>
2024-06-17 11:54:04 -0300

    Add files via upload

---
+++ exercício/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+            Console.WriteLine("Pão");
+
+
+        }
+    }
+}
